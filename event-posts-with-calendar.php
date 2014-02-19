4afb27b11ec285ff630942673e951632e45fa5a5
Parco dei Suoni <parcodeisuoni@gmail.com>
2014-02-19 13:35:50 +0100

    First file created
    First add

---
+++ event-posts-with-calendar.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: Event Posts with Calendar
+Plugin URI: http://parcodeisuoni.net/wp-tools/plugins
+Description: Creates an Event post type with custom date based archives, also includes a modified version of the default Wordpress calendar widget specific for Events, as well as a set of rewrite rules for pretty-permalinks.
+Version: 1.0
+Author: Marcello Liberato
+Author URI: http://parcodeisuoni.net/wp-tools/about
+License: GPLv2
+*/
+/*
+Copyright (C) 2013 Marcello Liberato, parcodeisuoni.net (parcodeisuoni AT gmail DOT com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+?>
